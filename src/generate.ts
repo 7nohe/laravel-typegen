07b42f2a3fa29c0a12c1d0d4fe173063b1ce03a7
7nohe <7nohe.urata@gmail.com>
2022-11-20 15:35:02 +0900

    feat: support relation

--- src/generate.ts
+++ src/generate.ts
@@ -6,7 +6,7 @@ import fs from "fs";
 import { execSync } from "child_process";
 import { LaravelModelType } from "./types";
 
-const tmpDir = './.laravel-typegen-tmp'
+const tmpDir = "./.laravel-typegen-tmp";
 export async function generate(options: CLIOptions) {
   const models = glob.sync(`app/Models/*.php`);
   const modelData: LaravelModelType[] = [];
@@ -14,13 +14,15 @@ export async function generate(options: CLIOptions) {
     fs.mkdirSync(tmpDir);
   }
   for (const model of models) {
-    const modelName = model.split('/').at(-1)?.replace('.php', '')
+    const modelName = model.split("/").at(-1)?.replace(".php", "");
     const modelShowCommand = `php artisan model:show ${modelName} --json > ${tmpDir}/${modelName}.json`;
     console.log(modelShowCommand);
     execSync(modelShowCommand);
     console.log(`${modelName}.json was created successfully`);
-    const modelJson = JSON.parse(fs.readFileSync(`${tmpDir}/${modelName}.json`, 'utf8')) as LaravelModelType
-    modelData.push(modelJson)
+    const modelJson = JSON.parse(
+      fs.readFileSync(`${tmpDir}/${modelName}.json`, "utf8")
+    ) as LaravelModelType;
+    modelData.push(modelJson);
   }
   fs.rmSync(tmpDir, { recursive: true });
   const source = createSource(modelData);
