22804418c95b62133ab233faf18f22a1d70a4073
7nohe <7nohe.urata@gmail.com>
2022-12-08 01:21:40 +0900

    feat: add ziggy option

--- examples/laravel9-app/resources/ts/types/route.d.ts
+++ examples/laravel9-app/resources/ts/types/route.d.ts
@@ -1,8 +1,8 @@
 import { Config, Router } from "ziggy-js";
 import { RouteParams } from "./param";
 declare global {
-     declare function route(): Router;
-     declare function route<T extends keyof RouteParams>(name: T, params?: RouteParams[T], absolute?: boolean, config?: Config): string;
+    declare function route(): Router;
+    declare function route<T extends keyof RouteParams>(name: T, params?: RouteParams[T], absolute?: boolean, config?: Config): string;
 }
 declare module "vue" {
     interface ComponentCustomProperties {
--- src/generate.ts
+++ src/generate.ts
@@ -1,12 +1,22 @@
 import { print } from "./print";
 import { CLIOptions } from "./cli";
 import { createSource as createModelSource } from "./models/createSource";
-import { createRouteDeclarationSource, createRouteParamsSource } from "./routes/createSource";
+import {
+  createRouteDeclarationSource,
+  createRouteParamsSource,
+} from "./routes/createSource";
 import glob from "glob";
 import fs from "fs";
 import { execSync } from "child_process";
 import { LaravelModelType, LaravelRouteListType } from "./types";
-import { defaultEnumPath, defaultOutputPath, defaultModelPath, modelFileName, routeParamsFileName, indexDeclarationFileName } from "./constants";
+import {
+  defaultEnumPath,
+  defaultOutputPath,
+  defaultModelPath,
+  modelFileName,
+  routeParamsFileName,
+  indexDeclarationFileName,
+} from "./constants";
 
 const tmpDir = "./.laravel-typegen-tmp";
 export async function generate(options: CLIOptions) {
@@ -28,23 +38,36 @@ export async function generate(options: CLIOptions) {
     modelData.push(modelJson);
   }
 
-  const modelSource = createModelSource(modelFileName, modelData, enums, options);
+  const modelSource = createModelSource(
+    modelFileName,
+    modelData,
+    enums,
+    options
+  );
   print(modelFileName, modelSource, options.output ?? defaultOutputPath);
 
-  // Generate routes
-  const routeListCommand = `php artisan route:list --json > ${tmpDir}/route.json`;
-  console.log(routeListCommand);
-  execSync(routeListCommand);
-  const routeJson = JSON.parse(
-    fs.readFileSync(`${tmpDir}/route.json`, "utf8")
-  ) as LaravelRouteListType[];
+  // Generate types for ziggy
+  if (options.ziggy) {
+    const routeListCommand = `php artisan route:list --json > ${tmpDir}/route.json`;
+    console.log(routeListCommand);
+    execSync(routeListCommand);
+    const routeJson = JSON.parse(
+      fs.readFileSync(`${tmpDir}/route.json`, "utf8")
+    ) as LaravelRouteListType[];
 
-  const routeSource = createRouteParamsSource(routeParamsFileName, routeJson, options);
-  
-  print(routeParamsFileName, routeSource, defaultOutputPath);
-  
-  const routeDeclarationSource = createRouteDeclarationSource(indexDeclarationFileName);
-  print(indexDeclarationFileName, routeDeclarationSource, defaultOutputPath);
+    const routeSource = createRouteParamsSource(
+      routeParamsFileName,
+      routeJson,
+      options
+    );
+
+    print(routeParamsFileName, routeSource, defaultOutputPath);
+
+    const routeDeclarationSource = createRouteDeclarationSource(
+      indexDeclarationFileName
+    );
+    print(indexDeclarationFileName, routeDeclarationSource, defaultOutputPath);
+  }
 
   fs.rmSync(tmpDir, { recursive: true });
 }
