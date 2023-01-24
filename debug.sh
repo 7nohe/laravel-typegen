cd40790264c116f5063466973dc6e499b61d18e6
7nohe <7nohe.urata@gmail.com>
2023-01-24 23:56:40 +0900

    feat: update debug script

--- debug.sh
+++ debug.sh
@@ -4,7 +4,7 @@ pnpm run build
 cd ./examples/laravel9-app
 ./vendor/bin/sail npm install
 ./vendor/bin/sail bash -c "rm -rf /var/www/html/node_modules/@7nohe/laravel-typegen"
-./vendor/bin/sail bash -c "cp -R /laravel-typegen /var/www/html/node_modules/@7nohe/laravel-typegen"
+./vendor/bin/sail bash -c "cp -R /laravel-typegen/dist /var/www/html/node_modules/@7nohe/laravel-typegen"
 ./vendor/bin/sail npm run typegen
 # ./vendor/bin/sail npm run typegen:laravel-enum
 
